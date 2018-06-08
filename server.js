a8e0cddcba21a8efebfcf9940c6126e8962d9e3f
hesam <hesamr76@gmail.com>
2018-06-08 17:35:12 +0430

    spagety code :D first test that crawl page 2 of jobinja

--- server.js
+++ server.js
@@ -1,55 +1,79 @@
 //requires 
-const express = require("express");
 const mongo = require("mongoose");
-const bodyParser = require("body-parser");
 const cheerio = require('cheerio');
 const fs = require("fs");
 const axios = require("axios");
+
+//global variables
 var $;
+var allData = [];
 
-
-var app = express();
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-
-var str = "";
-
+//#crawler start
 console.log("server started");
 
 
-function crawler(url, params, structObj) {
+function crawler(url, params, urlOfJobTarget , conditionsOfJobTarget) {
     axios.get(url, params)
-        .then(function (response) {
-            // console.log(response);
+        .then(function (response){
+            //  console.log(response);
             $ = cheerio.load(response.data)
             
-            getLinksOfJobs(structObj)
+            getLinksOfJobs(urlOfJobTarget,conditionsOfJobTarget)
+        })
 
-            // fs.writeFileSync("first-card-data1.txt", $('html').text());
-            // fs.writeFileSync("first-card-data.txt", String($().text()));
+        //update new jobs
+    storeData(allData)
+
+}
+
+crawler("https://jobinja.ir/jobs", {page: 2} , "h3.c-jobListView__title > a.c-jobListView__titleLink" , "li.c-infoBox__item div");
 
 
+function getLinksOfJobs(urlTarget,conditionTarget){
+
+    for(var i in $(urlTarget)) {
+        
+            if ($(urlTarget).eq(i).attr("href") != undefined){
+                let UrlOfThisJobForShowMore = $(urlTarget).eq(i).attr("href");
+                getdetailsOfLink(UrlOfThisJobForShowMore,conditionTarget)
+            }
+        
+    }
+}
+
+function getdetailsOfLink(url,target){
+    axios.get(url)
+        .then(function (response) {
+            //  console.log(response);
+            $ = cheerio.load(response.data)
+
+            let subject = "";
+            let items = [];
+
+            let dataOfThisJob = [];
+
+            $(target).each(function(index){
+
+                subject = $(this).prev().text();
+
+                $(this).find("span").each(function(){
+                    items.push($(this).text());
+                })   
+                
+                dataOfThisJob.push({url:url ,
+                                    info:{subject:subject,
+                                        items:items}}
+                                    )
+
+                
+
+                allData.push(dataOfThisJob)
+                
+            })
         })
 }
 
-
-crawler("https://jobinja.ir/jobs", {page: 2} , "h3.c-jobListView__title > a.c-jobListView__titleLink");
-
-function getdetailsOfLink(){
-    
+function storeData(data){
+    //connect to mongo and update
+    console.log(allData)
 }
-
-function getLinksOfJobs(structure){
-  
-    for(var i in $(structure)) {
-        // if (Number(i) == NaN) {
-        //     console.log($(structure).eq(i).attr("href"))
-        // }
-        console.log(Number(i))
-    }
-
-    getdetailsOfLink()
-}
-

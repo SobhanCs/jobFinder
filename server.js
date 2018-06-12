8ba2cdf6174ded5143920190f1fb545593e60083
hesam <hesamr76@gmail.com>
2018-06-12 23:45:47 +0430

    jobinja crowler

--- server.js
+++ server.js
@@ -12,68 +12,69 @@ var allData = [];
 console.log("server started");
 
 
-function crawler(url, params, urlOfJobTarget , conditionsOfJobTarget) {
+function crawler(url, params, urlOfJobTarget , liOfConditionsInJobTarget,targetOfTitleCondition,targetOfTagInTheTitle) {
     axios.get(url, params)
         .then(function (response){
             //  console.log(response);
             $ = cheerio.load(response.data)
             
-            getLinksOfJobs(urlOfJobTarget,conditionsOfJobTarget)
+            getLinksOfJobs(urlOfJobTarget,liOfConditionsInJobTarget,targetOfTitleCondition,targetOfTagInTheTitle)
         })
-
-        //update new jobs
-    storeData(allData)
 
 }
 
-crawler("https://jobinja.ir/jobs", {page: 2} , "h3.c-jobListView__title > a.c-jobListView__titleLink" , "li.c-infoBox__item div");
+crawler("https://jobinja.ir/jobs", {page: 2} , "h3.c-jobListView__title > a.c-jobListView__titleLink" , "li.c-infoBox__item", ".c-infoBox__itemTitle",".black");
 
 
-function getLinksOfJobs(urlTarget,conditionTarget){
+function getLinksOfJobs(urlTarget,liOfConditionTarget,targetOfTitleCondition,targetOfTagInTheTitle){
 
     for(var i in $(urlTarget)) {
         
             if ($(urlTarget).eq(i).attr("href") != undefined){
                 let UrlOfThisJobForShowMore = $(urlTarget).eq(i).attr("href");
-                getdetailsOfLink(UrlOfThisJobForShowMore,conditionTarget)
+                getUrlDetails(UrlOfThisJobForShowMore,liOfConditionTarget,targetOfTitleCondition,targetOfTagInTheTitle)
             }
         
     }
 }
 
-function getdetailsOfLink(url,target){
+function getUrlDetails(url,li,title,tag){//any li have a title and some tags --> title like : مهارت های مورد نیاز  and tags like : ux/sketch/css
     axios.get(url)
         .then(function (response) {
             //  console.log(response);
             $ = cheerio.load(response.data)
 
             let subject = "";
-            let items = [];
 
             let dataOfThisJob = [];
 
-            $(target).each(function(index){
+            $(li).each(function(i){
+                subject = $(this).find(title).text();
+                // console.log(subject);
+                let items = [];
 
-                subject = $(this).prev().text();
+                $(this).find(tag).each(function(){
+                    // console.log($(this).text());
+                    items.push($(this).text())
+                });
 
-                $(this).find("span").each(function(){
-                    items.push($(this).text());
-                })   
-                
-                dataOfThisJob.push({url:url ,
-                                    info:{subject:subject,
-                                        items:items}}
-                                    )
+                dataOfThisJob.push({
+                    subject : subject,
+                    items: items
+                })
 
-                
+            }) 
 
-                allData.push(dataOfThisJob)
-                
-            })
+            allData.push({url : url , data : dataOfThisJob})
+            storeData(allData);
+            
+
         })
+
 }
 
-function storeData(data){
+function storeData(data){//i don't know when call this function that allData synced
     //connect to mongo and update
-    console.log(allData)
+    console.log(data);
+
 }

827fb78a8b65cdac38ceee92880369c297f87bee
hesam <hesamr76@gmail.com>
2018-06-22 09:40:15 +0430

    make object for websites

--- scraper.js
+++ scraper.js
@@ -9,6 +9,29 @@ const axios = require("axios");
 //global variables
 let $, repeated = 0;
 
+var sources = {
+    "jobinja": {
+        "url": {
+            "prefix": "https://jobinja.ir/jobs?filters%5Bkeywords%5D%5B0%5D=&sort_by=published_at_desc&page=",
+            "suffix": "",
+            "page": 1
+        },
+        "target": {
+            "subject":".c-jobView__titleText",
+            "linksOfJob": "h3.c-jobListView__title > a.c-jobListView__titleLink",
+            "conditions": "li.c-infoBox__item",
+            "titleOfConditions": ".c-infoBox__itemTitle",
+            "tagInTitleOfConditions": ".black",
+            "expireDate": ".u-textCenter.u-textSmall.u-mB0 b",
+            "logoOfCompany": ".c-companyHeader__logoImage",
+            "description": ".o-box__text",
+            "companyName": ".c-companyHeader__name"
+        },
+        "lastCrawlDate": true,
+        lastJobThatSortedByDate:""
+    }
+}
+
 // crawler start
 console.log("Crawler started...");
 
@@ -25,7 +48,7 @@ db.once('connected', function () {
 });
 
 // defining jonSchema in mongodb
-let jobSchema =  new mongoose.Schema({
+let jobSchema = new mongoose.Schema({
     url: {
         type: String,
         require: true
@@ -35,8 +58,8 @@ let jobSchema =  new mongoose.Schema({
         require: true
     }, // use the link of job as id
     title: {
-        type: String,
-        require: true
+        type: String //,
+        //require: true
     },
     typeOfJob: String,
     location: String,
@@ -56,28 +79,13 @@ let jobSchema =  new mongoose.Schema({
         require: true
     },
     description: String,
-    expireDate: Date,
-    crawlTime: Date,
+    expireDate: String,
+    crawlTime: String,
     experience: String
 });
 
 let jobModel = mongoose.model("jobModel", jobSchema);
 
-// job.save(function (err, job) {     //save in mongobd
-//     if (err) {
-//         throw err
-//     }
-//     console.log(job)
-// });
-
-// jobModel.find({       //find in mongodb
-//     name: "job"
-// }, function (err, user) {
-//     if (err) {
-//         throw err
-//     }
-//     console.log("Found !", user)
-// });
 
 function generateUrl(prefixUrl, pageNumberUrl, suffixUrl, urlTarget) {
     let urlsArray = [];
@@ -89,10 +97,9 @@ function generateUrl(prefixUrl, pageNumberUrl, suffixUrl, urlTarget) {
                     urlsArray.push($(urlTarget).eq(item).attr("href"));
                 }
             }
-            // console.log(urlsArray);
-            // return urlsArray;
+
             urlsArray.forEach(item => {
-                getUrlDetails(item, "li.c-infoBox__item", ".c-infoBox__itemTitle", ".black");
+                getUrlDetails(item, sources.jobinja.target)
             });
 
 
@@ -100,119 +107,94 @@ function generateUrl(prefixUrl, pageNumberUrl, suffixUrl, urlTarget) {
 
 }
 
-function startCrawler(storeData) {
-    let page = 1;
+function startCrawler(website) {
 
-    // while (page < 3) { //repeated<10
+    while (website.url.page < 3) { //repeated<10
 
-    generateUrl("https://jobinja.ir/jobs?filters%5Bkeywords%5D%5B0%5D=&sort_by=published_at_desc&page=", page, "", "h3.c-jobListView__title > a.c-jobListView__titleLink")
+        generateUrl(website.url.prefix , website.url.page , website.url.suffix , website.target.linksOfJob)
 
-    // page++;
+        website.url.page ++;
 
-    // if (false) { //when a job already exist
-    // repeated++;
-    // }
-    // }
+        // if (false) { //when a job already exist
+        // repeated++;
+        // }
+    }
 }
 
-startCrawler(storeData);
+startCrawler(sources.jobinja);
 
-
-function getUrlDetails(url, li, title, tag, callBack) { //any li have a title and some tags --> title like : مهارت های مورد نیاز  and tags like : ux/sketch/css
-    axios.get(url)
+//this function get a link that is a new job ,this job need to reed data and target help us for select any items in detail
+function getUrlDetails(url , target) {
+    axios.get(url)//axios make request and get data of detail page
         .then(function (response) {
-            //  console.log(response);
-            $ = cheerio.load(response.data)
+            $ = cheerio.load(response.data)//cherio get data from axios and help us to select objects in html source like jquery
 
-            let subject = "";
+            let subject = "";//subject like : ...جنسیت و حداقل مدرک و حقوق و 
 
-            let dataOfThisLi = [];
+            let dataOfThisLi = [];//a array that have ["جنسیت","مرد"]
 
-            let finall = {url : url,
-                          id : "our detail url",
-                          crawlTime: "امروز",
-                          expireTime: "۲۰ روز"
+            let final = {//finall is an object that will append to data base
+                url: url,
+                id: "our detail url",
+                crawlTime: "امروز",
+                expireTime: $(target.expire).text().replace(/ روز/g, ''),
+                description: $(target.description).text(),
+                logoSource: $(target.logoOfCompany).attr("src"),
+                companyName: $(target.companyName).text(),
             }
 
-            $(li).each(function () {
-                subject = $(this).find(title).text();
-                // console.log(subject);
-                let items = [], data = {};
+            $(target.conditions).each(function () {
+                //fill subjects in foreach and get tags of this subject in next forEach
+                subject = $(this).find(target.titleOfConditions).text(); //like "مهارت ها"
 
-                $(this).find(tag).each(function () {
-                    // console.log($(this).text());
+                let items = [],//an array that have tags in a subject like ["ui/ux","html","css","js"]
+                    data = {};//this object join subject and their tags to url
+
+                $(this).find(target.tagInTitleOfConditions).each(function () {
                     items.push($(this).text())
                 });
 
-                dataOfThisLi.push({
+                dataOfThisLi.push({//join tags to subject
                     subject: subject,
                     items: items
                 })
-            })     
-                data = {
-                    url: url,
-                    preRequire: dataOfThisLi
-                }
-                //console.log(data);
+            })
+            data = {
+                url: url,
+                preRequire: dataOfThisLi
+            }
 
-                relation = [
-                            ["عنوان","title"],
-                            ["دسته بندی شغلی","typeOfJob"],
-                            ["موقعیت مکانی","location"],
-                            ["نوع همکاری","typeOfCollabration"],
-                            ["حقوق","Salary"],
-                            ["وضعیت نظام وظیفه","militeryService"],
-                            ["مهارت های مورد نیاز","skill"],
-                            ["جنسیت","sex"],
-                            ["رشته‌های تحصیلی مرتبط","relativeField"],
-                            ["حداقل مدرک تحصیلی","education"],
-                            ];
-                            
-                //console.log('{URL : "' + element.url + '",\nDATA: { ');
-                // str += '{URL : "' + element.url https://insiders.liveshare.vsengsaas.visualstudio.com/join?4E2F82AD9920860B4C3FD37AFE3F5649A8A6+ '",\nDATA: { \n';
-                data["preRequire"].forEach(function (dataElement) {
-                    //console.log('\tTitle : "' + dataElement.subject + ' " ,\n\tItems :');
-                    // str += '\tTitle : "' + dataElement.subject + ' " ,\n\tItems :\n';
-                    thisItems = [];
-                    dataElement["items"].forEach(function (items) { // Use forEach for tags
-                        //console.log("\t" + items + "\n}\n}");
-                        // str += "\t" + items + "\n"                 
-                        thisItems.push(items.trim().replace(/  /g,''))
-                    })
+            //we make relation array to convert lang :D get titles from site and make field in database
+            relation = [
+                ["عنوان", "title"],
+                ["دسته بندی شغلی", "typeOfJob"],
+                ["موقعیت مکانی", "location"],
+                ["نوع همکاری", "typeOfCollabration"],
+                ["حقوق", "Salary"],
+                ["وضعیت نظام وظیفه", "militeryService"],
+                ["مهارت های مورد نیاز", "skill"],
+                ["جنسیت", "sex"],
+                ["رشته‌های تحصیلی مرتبط", "relativeField"],
+                ["حداقل مدرک تحصیلی", "education"],
+            ];
 
-                    relation.forEach(item => {
-                        if(item[0] == dataElement.subject){
-                            finall[item[1]] = thisItems;                   
-                        }                   
-                    })
-                    
-                    
+            data["preRequire"].forEach(function (dataElement) {
+                thisItems = [];
+                dataElement["items"].forEach(function (items) { // Use forEach for tags
+                   thisItems.push(items.trim().replace(/  /g, ''))//get items in title and remove whitespace
                 })
-                console.log(finall);
-                jobModel.insertMany(finall,
-                   function () {
-                });
-        //str += "------------------------------------------"
-        //console.log("----------------------------------------");
-        //fs.appendFileSync("first-card-data",str);
 
-        // "url : " + element.url + "\n"
-        //console.log(element.data.forEach({}));
-
-                // console.log(allData)
-
-    }) //.done(function () {
-            //storeData(allData) BAD PLACE :(
-            // })
-
-}
+                relation.forEach(item => {
+                    if (item[0] == dataElement.subject) {
+                        final[item[1]] = thisItems;//add other field and data to finall 
+                    }
+                })
 
 
-
-function storeData(data) { // i don't know when call this function that allData synced
-    // connect to mongo and update
-    // console.log(data);
-    // fs.writeFileSync("first-card-data",data)
-
-    
+            })
+            //log finall and add to database - final is an object of a job
+            console.log(final);
+            jobModel.insertMany(final,
+                function () {});
+        }) 
 }

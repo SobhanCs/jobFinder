b1ba967d62f8e66cf32b36292fcbe69752a08f78
Little User <erfaneivaziq14@gmail.com>
2018-06-29 17:03:46 +0430

    Ui cards

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,9 @@ document.addEventListener('DOMContentLoaded', function() {
 //   $(document).ready(function(){
 //     $('.sidenav').sidenav();
 //   });
+
+
+var elem = document.querySelector('.collapsible.expandable');
+var instance = M.Collapsible.init(elem, {
+  accordion: false
+});

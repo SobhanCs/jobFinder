16b262e191c420a926ec29e8f099271c2e93884f
hesam <hesamr76@gmail.com>
2018-07-13 00:10:53 +0430

    lastest version of scraper and panel view just need to replace routing to server.js for show in dashboard

--- public/js/crawlerPanel.js
+++ public/js/crawlerPanel.js
@@ -3,7 +3,7 @@ let currentPage = 1;
 let pageNumber = 1;
 
 $("#getnewJobs").on("click", function () {
-    let url = "/newjobs/" + 1
+    let url = "/newjobs/" + currentPage
     $.ajax({
         url: url,
         method: "get"
@@ -18,21 +18,26 @@ function createNewJobs(json) {
     $("#items").html("")
     $("#items").hide()
     for (let index in json) {
-        // console.log(json[index]);
+        console.log(json[index]);
 
         $("#items").append(
             '<div class="col-12 p-2 text-right">' +
-            '<div id = "jobId_' + index + '" class="shadow card text-white bg-secondary mb-3">' +
-            '<div class="card-header col-12 " onclick="toggle(' + index + ')">' +
-            '<div class="col-6 p-2" style="float:right"><a class="btn btn-info col-12" role="button" href="' + json[index].url + '" target="_blank">' +
+            '<div id = "jobId_' + index + '" class="shadow card text-white bg-secondary mb-3 parent rounded">' +
+            '<div class="card-header col-12" onclick="toggle(' + index + ',event)" style="cursor:pointer;border:none">' +
+            '<div class="col-6 p-2 parent" style="float:right"><a class="btn btn-info col-12 Hover" role="button" href="' + json[index].url + '" target="_blank">' +
             '<p class="secondary ml-2 mb-0" style="float:right"> - ' + (+index + 1) + '</p>' +
             '<p style="float:right;vertical-align:middle;margin-bottom:0;">' + json[index].title + '</p></a></div>' +
-            '<div class="col-3 p-2" style="float:left" ><button onclick="addToArchive(' + index + ')" type="button" class="btn btn-danger col-12">پنهان</button></div>' +
-            '<div class="col-3 p-2" style="float:left" ><button onclick="addToDatabase(' + index + ')" type="button" class="btn btn-success col-12">تایید</button></div></div>' +
+            '<div class="col-3 p-2 parent" style="float:left" ><button onclick="addToArchive(' + index + ')" type="button" class="btn btn-danger col-12 Hover">پنهان</button></div>' +
+            '<div class="col-3 p-2 parent" style="float:left" ><button onclick="addToDatabase(' + index + ')" type="button" class="btn btn-success col-12 Hover">تایید</button></div>'+
+            '<div class="col-12 mt-2 d-flex justify-content-center">&#8645;</div></div>' +
 
             '<div class = "card-body row" id="body_' + index + '">' +
-            '<img width="100%" height="150px" class="col-2" src="' + json[index].logoSource + '" style="float:left"></img>' +
-            '<div class="col-5"><h6>وضعیت نظام وضیفه</h6>' +
+            '<div class = "col-2">'+
+            '<h6>منبع</h6>' +
+            '<p>'+json[index].siteName+'</p><br>'+
+            '<h6>لوگو شرکت</h6>' +
+            '<img width="100%" height="150px" src="' + json[index].logoSource + '" style="float:left"></img>' +
+            '</div><div class="col-5"><h6>وضعیت نظام وضیفه</h6>' +
             '<p class = "card-text" >' + json[index].militeryService + '</p>' +
             '<h6>موقعیت مکانی</h6>' +
             '<p class = "card-text" >' + json[index].location + '</p>' +
@@ -68,7 +73,9 @@ function createNewJobs(json) {
     $("#items").slideDown(1000)
 }
 //pagination jquery nodejs
+//use jquery for make pagination DOM
 function makePagination() {
+    $(".items").html("")
     let pages = Math.ceil($(".badge").text() / 15)
     pageNumber = pages;
     if (pages > 1) {
@@ -84,7 +91,8 @@ function makePagination() {
         $(".items").append('<a onclick="nextPage()" >&raquo;</a>')
     }
 }
-
+//<<<<<<>>>>>><<<<<<>>>>>><<<<<<>>>>>><<<<<<>>>>>>
+//call API for get jobs of current page 
 function getThisPage(page) {
     currentPage = page
     let url = "/newjobs/" + page
@@ -100,19 +108,23 @@ function getThisPage(page) {
         outputJSON = data
     });
 }
-
+//<<<<<<>>>>>><<<<<<>>>>>><<<<<<>>>>>><<<<<<>>>>>>
+//next sign in pagination call this function
 function nextPage() {
     if (currentPage != pageNumber) {
         getThisPage(currentPage + 1)
     }
 }
-
+//<<<<<<>>>>>><<<<<<>>>>>><<<<<<>>>>>><<<<<<>>>>>>
+//previos sign in pagination call this function
 function prevPage() {
     if (currentPage != 1) {
         getThisPage(currentPage - 1)
     }
 }
 //end pagination
+//<<<<<<>>>>>><<<<<<>>>>>><<<<<<>>>>>><<<<<<>>>>>>
+//onclick for button (پنهان) => use ajax and connect to database
 function addToArchive(index) {
 
     $.ajax({
@@ -122,10 +134,11 @@ function addToArchive(index) {
         data: JSON.stringify(outputJSON[index]),
     }).then(function () {
         console.log("this job added to archive");
-        $("#items > div").eq(index).children().removeClass("bg-secondary").removeClass("bg-success").addClass("bg-danger")
+        $("#items > div").eq(index).children().removeClass("bg-secondary").removeClass("bg-success").addClass("bg-danger",6000)
     })
 }
-
+//<<<<<<>>>>>><<<<<<>>>>>><<<<<<>>>>>><<<<<<>>>>>>
+//onclick for button (تایید) => use ajax and connect to database
 function addToDatabase(index) {
 
     $.ajax({
@@ -135,10 +148,12 @@ function addToDatabase(index) {
         data: JSON.stringify(outputJSON[index]),
     }).then(function () {
         console.log("this job added to database");
-        $("#items > div").eq(index).children().removeClass("bg-secondary").removeClass("bg-danger").addClass("bg-success")
+        $("#items > div").eq(index).children().removeClass("bg-secondary").removeClass("bg-danger").addClass("bg-success",6000)
     })
 }
-
-function toggle(card) {
-    $("#body_" + card).slideToggle(1300)
+//<<<<<<>>>>>><<<<<<>>>>>><<<<<<>>>>>><<<<<<>>>>>>
+//onclick on header of jobs for slide in and show more
+function toggle(card,e) {
+    if(e.target.innerHTML != "پنهان" & e.target.innerHTML != "تایید")
+    $("#body_" + card).slideToggle(900)
 }

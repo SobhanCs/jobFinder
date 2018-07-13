2125cc2b87397a956895f38640d60fa828f009eb
Little User <erfaneivaziq14@gmail.com>
2018-07-13 08:15:15 +0430

    Ui works, Sync commit

--- app/routes.js
+++ app/routes.js
@@ -4,9 +4,9 @@ module.exports = function (app, passport) {
     // =====================================
     // HOME PAGE (with login links) ========
     // =====================================
-    app.get('/', function (req, res) {
-        res.render('index.ejs'); // load the index.ejs file
-    });
+    // app.get('/', function (req, res) {
+    //     res.render('index.ejs'); // load the index.ejs file
+    // });
 
 
     // =====================================
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var mongoose     = require('mongoose');
 // var port         = process.env.PORT || 8580;
 var passport     = require('passport');
 var flash        = require('connect-flash');
-var morgan       = require('morgan');
+// var morgan       = require('morgan');
 var bodyParser   = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session      = require('express-session');
@@ -25,7 +25,7 @@ mongoose.connect(configDB.url); // connect to our database
 require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
-app.use(morgan('dev')); // log every request to the console
+// app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 // app.use(bodyParser('application/json')); // get information from html forms
 app.use(bodyParser.urlencoded({
@@ -65,7 +65,6 @@ app.use('/', router);
 
 // homepage page 
 router.get('/', function (req, res) {
-    console.log(__dirname);
     res.render('index');
 });
 
@@ -82,6 +81,23 @@ router.get('/all/:page', function (req, res) {
             });
         });
 });
+
+router.get('/search', function (req, res) {
+    console.log(req.query);
+    let Page;
+    if (req.query.page == null)
+        Page = 1;
+    jobModel.find({ title: req.query.q }, function (err, jobs) {
+        console.log(jobs);
+        return jobs;
+    }).skip((Page-1)*10).limit(10)
+    .then(function (jobs) {
+        res.render('result', {
+            Jobs: jobs,
+            page: Page
+        });
+    });
+})
 
 // router.get('*', function (req, res) {
 //     res.send(404);

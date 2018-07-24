9e6cb5e679ece661ef9d8612adab47541e21ec67
hesam <hesamr76@gmail.com>
2018-07-24 18:48:44 +0430

    crawler can crawl jobinja jobvision and karboom and in database already exists

--- scraper.js
+++ scraper.js
@@ -19,8 +19,9 @@ const axios = require("axios");
 let $, repeated = 0,
   json = {},
   news = 0;
-  turn = 0
-var sites = ["jobinja","jobvision","karboom","sokanacademy"]
+turn = 0
+
+var sites = ["jobinja", "jobvision", "karboom"]
 
 var sources = {
   "jobinja": {
@@ -43,13 +44,13 @@ var sources = {
       "siteName": "jobinja"
     },
     "lastCrawlDate": true,
-    lastJobThatSortedByDate: ""
+    "lastJobThatSortedByDate": ""
 
   },
   "jobvision": {
     "url": {
       "url": "https://jobvision.ir/JobPost/GetJobPostListData",
-      "body": `page=${this.page}&SortBy=0&JobTitle=&SelectedCity=&SelectedIndustrial=&SelectedLevelOfSeniority=&SelectedJobGroup=&SelectedWorkType=&SelectedWorkExprience=&MinMatchingPercent=0&MaxMatchingPercent=0&pageSize=40&IsForJobFair=false`,
+      "prefix": "https://jobvision.ir",
       "page": 1
     },
     "target": {
@@ -57,16 +58,19 @@ var sources = {
       "linksOfJob": ".jobpost-box:not(.headBox)",
       "jobBox": "div.jobpostmainbox",
       "conditions": "#third",
-      "titles": ".title-text-size",
+      "titles": ".title-text-size:not(.blue)",
       "values": ".value-text-size",
       "logoOfCompany": "img.center-block",
-      // "companyName": ".title-text-size.jobtitle",
+      "companyName": ".title-text-size.jobtitle",
+      "skill":"#third div:nth-child(6) .softwares-info-block.ref-software span:first-child",
+      "sex":"#third > div:nth-child(2) .value-text-size",
+      "minExperience":"#third > div:nth-child(3) .value-text-size",
+      "education":"#third > div:nth-child(4)  .value-text-size",
       "jobPerPage": 40,
       "siteName": "jobvision"
     },
     "lastCrawlDate": true,
-    lastJobThatSortedByDate: ""
-
+    "lastJobThatSortedByDate": ""
   },
   "karboom": {
     "url": {
@@ -92,46 +96,26 @@ var sources = {
       "siteName": "karboom"
     },
     "lastCrawlDate": true,
-    lastJobThatSortedByDate: ""
+    "lastJobThatSortedByDate": ""
 
   },
   "sokanacademy": {
-    "url": {
-      "prefix": "http://jobvision.ir/JobPost/Search?page=",
-      "suffix": "&SortBy=0&JobTitle&SelectedCity&SelectedIndustrial&SelectedLevelOfSeniority=&SelectedJobGroup=&SelectedWorkType=&SelectedWorkExprience=&MinMatchingPercent=0&MaxMatchingPercent=0&pageSize=40&IsForJobFair=false",
-      "page": 1
-    },
-    "target": {
-      "subject": ".value-text-size.jobtitle",
-      "linksOfJob": ".jobpost-box:not(.headBox)",
-      "jobBox": "div.jobpostmainbox",
-      "conditions":"#third",
-      "titles": ".title-text-size",
-      "values": ".value-text-size",
-      "logoOfCompany": "img.center-block",
-      // "companyName": ".title-text-size.jobtitle",
-      "jobPerPage": 40,
-      "siteName": "jobvision"
-    },
-    "lastCrawlDate": true,
-    lastJobThatSortedByDate: ""
-
   }
 }
 
 // crawler start
-console.log("Crawler ready...");
+console.log("Crawler ready...\n");
 
 // Connect to MongoDB
 mongoose.connect("mongodb://localhost:27017/jobteam");
 let db = mongoose.connection;
 
 // mongodb status
-db.on('error', function () {
+db.on('error', function() {
   console.log("We are not connected to MongoDB !");
 });
-db.once('connected', function () {
-  console.log("We are connected to MongoDB !");
+db.once('connected', function() {
+  console.log("We are connected to MongoDB !\n");
 });
 
 const jobModel = require('./app/models/jobModel'); // the name of collection by erfan
@@ -139,7 +123,7 @@ const counter = require('./app/models/counterModel'); // for auto-increment id f
 
 counter.findById({
   _id: 'entityId'
-}, function (err, res) {
+ }, function(err, res) {
   if (err) throw err;
   if (res == null) {
     new counter({
@@ -155,7 +139,7 @@ function generateUrl(source, url, target) {
   let urlsArray = [];
   if (target.siteName == "jobvision") {
     axios.post(url.url, {
-        page: 2,
+        page: url.page,
         SortBy: 0,
         JobTitle: '',
         SelectedCity: '',
@@ -168,11 +152,11 @@ function generateUrl(source, url, target) {
         MaxMatchingPercent: 0,
         pageSize: 40,
         IsForJobFair: false,
-        validateStatus: function (status) {
+        validateStatus: function(status) {
           return status < 500; // Reject only if the status code is greater than or equal to 500
         }
       }) // put a request to a url and get its html source
-      .then(function (response) {
+      .then(function(response) {
         $ = cheerio.load(response.data); // render received html source to can working it as a jquery syntax
         let statusCode = response.status
 
@@ -180,25 +164,26 @@ function generateUrl(source, url, target) {
 
         for (let item in $(target.linksOfJob)) { // loop on all our target items
           if (Number.isInteger(+item)) { // filter only urls in page - urls' name are explicitly a number
-            urlsArray.push($(target.linksOfJob).eq(item).attr("href")); // read href attribute of tag 'a' and push it into output array
+            urlsArray.push(url.prefix + $(target.linksOfJob).eq(item).attr("data-href")); // read href attribute of tag 'a' and push it into output array
           }
         }
 
         target.jobPerPage = urlsArray.length
 
-        console.log("number of jobs in this page is : " + target.jobPerPage);
+        console.log("number of jobs in this page is : " + target.jobPerPage +'\n');
 
         getUrlDetails(source, urlsArray, target)
 
       })
 
-  } else {
+  }
+  else {
     axios.get(url.prefix + url.page + url.suffix, {
-        validateStatus: function (status) {
+        validateStatus: function(status) {
           return status < 500; // Reject only if the status code is greater than or equal to 500
         }
       }) // put a request to a url and get its html source
-      .then(function (response) {
+      .then(function(response) {
         $ = cheerio.load(response.data); // render received html source to can working it as a jquery syntax
         let statusCode = response.status
 
@@ -212,7 +197,7 @@ function generateUrl(source, url, target) {
 
         target.jobPerPage = urlsArray.length
 
-        console.log("number of jobs in this page is : " + target.jobPerPage);
+        console.log("number of jobs in this page is : " + target.jobPerPage + '\n');
 
         getUrlDetails(source, urlsArray, target)
 
@@ -222,16 +207,18 @@ function generateUrl(source, url, target) {
 
 //main call
 // generateUrl(sources.jobinja, sources.jobinja.url, sources.jobinja.target)
-function start(json,site){
-  if (site){
-    console.log("start scrap in " + site);
+function start(json, site) {
+  if (site) {
+    index = 0;
+    repeated = 0;
+    console.log("****** start scrap in " + site+' ***********\n');
     generateUrl(json[site], json[site].url, json[site].target)
-  }else{
-    console.log("The End")
+  } else {
+    console.log("----------------------\n--------The End-------\n----------------------")
   }
 }
 
-start(sources , sites[turn])
+start(sources, sites[turn])
 
 
 var index = 0; //start crawl job with index
@@ -241,11 +228,11 @@ function getUrlDetails(object, urls, target) {
   let url = urls[index];
 
   axios.get(url, {
-      validateStatus: function (status) {
+      validateStatus: function(status) {
         return status < 500; // Reject only if the status code is greater than or equal to 500
       }
     }) //axios make request and get data of detail page
-    .then(function (response) {
+    .then(function(response) {
       $ = cheerio.load(response.data) //cherio get data from axios and help us to select objects in html source like jquery
       let statusCode = response.status
       let final = {}
@@ -269,7 +256,7 @@ function getUrlDetails(object, urls, target) {
           siteName: target.siteName
         }
 
-        $(target.conditions).each(function () {
+        $(target.conditions).each(function() {
           //fill subjects in foreach and get tags of this subject in next forEach
           subject = $(this).find(target.titleOfConditions).text(); //like "مهارت ها"
 
@@ -277,7 +264,7 @@ function getUrlDetails(object, urls, target) {
             data = {}; //this object join subject and their tags to url
 
 
-          $(this).find(target.tagInTitleOfConditions).each(function () {
+          $(this).find(target.tagInTitleOfConditions).each(function() {
             items.push($(this).text())
           });
 
@@ -306,9 +293,9 @@ function getUrlDetails(object, urls, target) {
           ["حداقل سابقه کار", "minExperience"]
         ];
 
-        data["preRequire"].forEach(function (dataElement) {
+        data["preRequire"].forEach(function(dataElement) {
           thisItems = [];
-          dataElement["items"].forEach(function (items) { // Use forEach for tags
+          dataElement["items"].forEach(function(items) { // Use forEach for tags
             thisItems.push(items.trim().replace(/  /g, '')) //get items in title and remove whitespace
           })
 
@@ -319,7 +306,8 @@ function getUrlDetails(object, urls, target) {
           })
 
         })
-      } else if (target.siteName == "karboom") {
+      }
+      else if (target.siteName == "karboom") {
         final = { //finall is an object that will append to data base
           url: url,
           id: 0,
@@ -340,17 +328,17 @@ function getUrlDetails(object, urls, target) {
           descriptionOfJob: [],
           siteName: target.siteName,
         }
-        $(target.skill).each(function () {
+        $(target.skill).each(function() {
           if (typeof $(this).prev().text() === "string") {
             final['skill'].push($(this).prev().text())
           }
         })
-        $(target.relativeField).each(function () {
+        $(target.relativeField).each(function() {
           if (typeof $(this).text() === "string") {
             final['relativeField'].push($(this).text())
           }
         })
-        $(target.descOfJob).each(function () {
+        $(target.descOfJob).each(function() {
           if (typeof $(this).text() === "string") {
             final['descriptionOfJob'].push($(this).text())
           }
@@ -358,100 +346,163 @@ function getUrlDetails(object, urls, target) {
 
 
       }
+      else if (target.siteName == "jobvision") {
+        final = {
+          url: url,
+          id: 0,
+          visibility: "NEW",
+          crawlTime: new Date().toJSON(),
+          logoSource: $(target.logoOfCompany).attr("src"),
+          companyName: $(target.companyName).text().trim().replace(/  /g, ''),
+          title: $(target.subject).text().trim().replace(/  /g, ' ').split('\n')[0],
+          siteName: target.siteName,
+          typeOfJob:'',
+          typeOfCollaboration:'',
+          location:'',
+          descriptionOfJob:'',
+          skill : [],
+          sex:$(target.sex).eq(1).text().trim(),
+          militeryService:$(target.sex).eq(2).text().trim(),
+          minExperience:$(target.minExperience).text().trim(),
+          education:$(target.education).eq(0).text().trim()
+        }
+
+        $(target.skill).each(function(){
+          if (typeof $(this).text() === "string") {
+            final['skill'].push($(this).text())
+          }
+        })
+
+        let  title = [];
+        let detail = {}
+
+        $(target.jobBox + " " +target.titles).each(function(index){
+          if (index != 0) {
+            title.push($(this).text().trim())
+          }else{
+
+          }
+        })
+
+        $(target.jobBox + " " +target.values).each(function(index){
+          if (index != 0) {
+            detail[title[index-1]] = $(this).text().trim()
+          }else{
+
+          }
+        })
+
+        let relation = [
+          ["صنعت :", "typeOfJob"],
+          ["نوع همکاری :", "typeOfCollaboration"],
+          ["محل کار :", "location"],
+          ["شرح شغل و وظایف", "descriptionOfJob"]
+        ];
+
+        for (var key in detail) {
+          relation.forEach(function(item){
+            if (item[0] == key) {
+              final[item[1]] = detail[key]; //add other field and data to finall
+            }
+          })
+        }
+      }
+
+      // console.log(final);
 
       index++;
 
-      jobModel.findOne({
-          "url": final.url
-        }, {
-          "_id": 0,
-          "url": 1
-        }, function (err, item) {
-          if (err) {
-            console.log(">>>>>>>>>>>>>>>>>>>>>>> Database Error: cant find url of undefined " + err);
-          }
-
-
-          if (item == null && statusCode == 200) {
-
-            counter.findByIdAndUpdate({
-              _id: "entityId"
-            }, {
-              $inc: {
-                seq: 0.5
-              }
-            }, function (err, res) {
-              if (err) throw err;
-
-              final.id = res.seq; // auto-increment id for our url
-
-            }).then(function (res) {
-
-              jobModel.insertMany(final,
-                function (err, doc) {
-                  json[news] = final
-                  news++
-
-                  console.log("job state " + index + " added !  /  with status code : " + statusCode);
-
-                  if (index < target.jobPerPage) {
-                    getUrlDetails(object, urls, target)
-                  } else {
-                    index = 0;
-                    console.log("----------------------------------");
-
-                    if (object.url.page < 200) {
-                      object.url.page++;
-                      generateUrl(object, object.url, target)
-                    } else {
-                      console.log("scrap " + target.siteName + " done!!");
-                      turn ++;
-                      start(sources , sites[turn])
-                    }
-                  }
-                })
-            });
-
-          } else {
-            repeated++;
-            console.log("repeted job!!  --> " + repeated);
-            if (repeated < 10) { //limitForRepeated
-              if (index < target.jobPerPage) {
-                getUrlDetails(object, urls, target)
-              } else {
-                index = 0;
-                console.log("----------------------------------");
-
-                if (object.url.page < 200) {
-                  object.url.page++;
-                  generateUrl(object, object.url, target)
-                } else {
-                  console.log("scrap " + target.siteName + " done!!");
-                  turn ++;
-                  start(sources , sites[turn])
-                }
-              }
-            } else {
-              console.log("can not find new job from" + target.siteName);
-              turn ++ ;
-              start(sources , sites[turn])
-            }
-          }
-        })
-        .catch(function (error) {
-          // handle error
-          console.log(error);
-        })
+      // jobModel.findOne({
+      //     "url": final.url
+      //   }, {
+      //     "_id": 0,
+      //     "url": 1
+      //   }, function(err, item) {
+      //     if (err) {
+      //       console.log(">>>>>>>>>>>>>>>>>>>>>>> Database Error: cant find url of undefined " + err);
+      //     }
+      //
+      //
+      //     if (item == null && statusCode == 200) {
+      //
+      //       counter.findByIdAndUpdate({
+      //         _id: "entityId"
+      //       }, {
+      //         $inc: {
+      //           seq: 0.5
+      //         }
+      //       }, function(err, res) {
+      //         if (err) throw err;
+      //
+      //         final.id = res.seq; // auto-increment id for our url
+      //
+      //       }).then(function(res) {
+      //
+      //         jobModel.insertMany(final,
+      //           function(err, doc) {
+      //             json[news] = final
+      //             news++
+      //
+      //             console.log("job state " + index + " added !  /  with status code : " + statusCode);
+      //
+      //             if (index < target.jobPerPage) {
+      //               getUrlDetails(object, urls, target)
+      //             } else {
+      //               index = 0;
+      //               console.log("----------------------------------\n");
+      //
+      //               if (object.url.page < 200) {
+      //                 object.url.page++;
+      //                 generateUrl(object, object.url, target)
+      //               } else {
+      //                 console.log("scrap " + target.siteName + " done!!\n");
+      //                 turn++;
+      //                 start(sources, sites[turn])
+      //               }
+      //             }
+      //           })
+      //       });
+      //
+      //     } else {
+      //       repeated++;
+      //       console.log("repeted job!!  --> " + repeated);
+      //       if (repeated < 10) { //limitForRepeated
+      //         if (index < target.jobPerPage) {
+      //           getUrlDetails(object, urls, target)
+      //         } else {
+      //           index = 0;
+      //           console.log("----------------------------------\n");
+      //
+      //           if (object.url.page < 200) {
+      //             object.url.page++;
+      //             generateUrl(object, object.url, target)
+      //           } else {
+      //             console.log("scrap " + target.siteName + " done!!\n");
+      //             turn++;
+      //             start(sources, sites[turn])
+      //           }
+      //         }
+      //       } else {
+      //         console.log("can not find new job from " + target.siteName + '\n');
+      //         turn++;
+      //         start(sources, sites[turn])
+      //       }
+      //     }
+      //   })
+      //   .catch(function(error) {
+      //     // handle error
+      //     console.log(error);
+      //   })
     })
 }
 
 var currentPage = 1;
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
 
   jobModel.find({
     "visibility": "NEW"
-  }).count(function (err, result) {
+  }).count(function(err, result) {
     if (err)
       console.log(">>>>>>>>>>>>>>>>>>>>>>> Database Error: cant find url of undefined " + err);
 
@@ -461,7 +512,7 @@ app.get('/', function (req, res) {
   })
 });
 
-app.get('/newjobs/:page', function (req, res) {
+app.get('/newjobs/:page', function(req, res) {
   let page = req.params.page
   let pageSize = 15
 
@@ -469,7 +520,7 @@ app.get('/newjobs/:page', function (req, res) {
 
   jobModel.find({
     "visibility": "NEW"
-  }, function (err, json) {
+  }, function(err, json) {
     if (err)
       console.log(">>>>>>>>>>>>>>>>>>>>>>>Database Error: cant find url of undefined " + err);
 
@@ -478,7 +529,7 @@ app.get('/newjobs/:page', function (req, res) {
   }).skip(pageSize * (page - 1)).limit(pageSize)
 });
 
-app.post('/addNew', function (req, res) {
+app.post('/addNew', function(req, res) {
   console.log("new job visible");
   let newJob = JSON.parse(JSON.stringify(req.body));
 
@@ -488,7 +539,7 @@ app.post('/addNew', function (req, res) {
     $set: {
       "visibility": "visible"
     }
-  }, function (err, item) {
+  }, function(err, item) {
     if (err)
 
       console.log(item);
@@ -498,7 +549,7 @@ app.post('/addNew', function (req, res) {
   res.redirect('/newjobs/' + currentPage);
 });
 
-app.post('/newArchive', function (req, res) {
+app.post('/newArchive', function(req, res) {
   console.log("new job hidden");
   let newArchive = JSON.parse(JSON.stringify(req.body));
 
@@ -508,7 +559,7 @@ app.post('/newArchive', function (req, res) {
     $set: {
       "visibility": "hidden"
     }
-  }, function (err, item) {
+  }, function(err, item) {
     if (err)
 
       console.log(item);
